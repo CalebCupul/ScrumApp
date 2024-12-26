d51ba8ae22678308f8d02fa936a2d71e19de42ab
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2024-12-26 16:15:05 -0600

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Caleb Cupul on 26/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
