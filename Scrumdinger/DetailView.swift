8a4b89b4d15370e773b99d3f9649df04dd87c886
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2025-01-08 13:42:14 -0600

    feat: add DetailView

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,47 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by Caleb Cupul on 08/01/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    let scrum: DailyScrum
+    
+    var body: some View {
+        List {
+            Section(content: {
+                Label("Start Meeting", systemImage: "timer")
+                    .font(.headline)
+                    .foregroundColor(.accentColor)
+                HStack {
+                    Label("Length", systemImage: "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minutes")
+                }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+            }, header: {
+                Text("Meeting Info")
+            })
+        }
+    }
+}
+
+#Preview {
+    DetailView(scrum: DailyScrum.sampleData[0])
+    
+    
+    
+}
